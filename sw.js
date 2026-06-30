e3c1b347781fe700947dc188d68d9f3be49afddb
TheOldSmokeArcade1984 <gianmarcofrattina@gmail.com>
2026-06-30 15:24:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,28 @@
-const CACHE_NAME = 'arcade-os-v6';
+const CACHE_NAME = 'arcade-os-v7';
 const urlsToCache = [
   './index.html',
   './manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
